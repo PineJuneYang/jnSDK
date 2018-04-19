95147ee795435e7d8afde059a67117806e3df270
yjncoder@163.com <yjncoder@163.com>
2018-04-19 20:46:18 +0800

    Initial commit

---
+++ app/src/main/java/com/jn/jnsdk/fragment/MineFragment.java
@@ -0,0 +1,36 @@
+package com.jn.jnsdk.fragment;
+
+import com.jn.jnsdk.R;
+
+/**
+ * Created by jn
+ * date on 2018/4/2.
+ */
+
+public class MineFragment extends BaseFragment {
+    @Override
+    protected int getContentViewLayoutID() {
+        return R.layout.fragment_mine;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
---
+++ app/src/main/java/com/jn/jnsdk/fragment/RouteFragment.java
@@ -0,0 +1,35 @@
+package com.jn.jnsdk.fragment;
+
+import com.jn.jnsdk.R;
+
+/**
+ * Created by jn
+ * date on 2018/4/2.
+ */
+
+public class RouteFragment extends BaseFragment {
+    @Override
+    protected int getContentViewLayoutID() {
+        return R.layout.fragment_route;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
